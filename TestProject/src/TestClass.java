0cb8dc88384e4bff2d2f8687da9486be1ef1d9e4
Mahi <Mahi@DESKTOP-6C528QH>
2019-07-28 13:30:43 +0530

    second merged

--- TestProject/src/TestClass.java
+++ TestProject/src/TestClass.java
@@ -4,5 +4,9 @@ public class TestClass {
 		System.out.println("How");
 		System.out.println("r");
 		System.out.println("you");
+		System.out.println("friend");
+		System.out.println("I");
+		System.out.println("am");
+		System.out.println("fine");
 	}
 }
