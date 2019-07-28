6983d69e949a5db3140b22bc1bf7493f7fa57e2b
Mahi <Mahi@DESKTOP-6C528QH>
2019-07-28 13:25:25 +0530

    merged changes

--- TestProject/src/TestClass.java
+++ TestProject/src/TestClass.java
@@ -1,6 +1,11 @@
 
 public class TestClass {
 	public static void main(String[] args) {
-		System.out.println("Hi");
+		System.out.println("How");
+		System.out.println("r");
+		System.out.println("you");
+		System.out.println("I");
+		System.out.println("am");
+		System.out.println("fine.");
 	}
 }
